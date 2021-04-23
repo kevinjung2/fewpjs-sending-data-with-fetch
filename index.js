b73216c226a58e9d6aee38a8c82b5935e8bd0879
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-22 21:05:28 -0400

    commit tree bc5bed67b3c3800345a088b9ed6a907966b76986

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })})
+  .then(r => r.json())
+  .then(appendId)
+  .catch(appendError)
+}
+
+function appendId(jsob) {
+  let ul = document.getElementById('list')
+  let li = document.createElement('li')
+  li.innerHTML = jsob.id
+  ul.append(li)
+}
+
+function appendError(error) {
+  let ul = document.getElementById('list')
+  let li = document.createElement('li')
+  li.innerHTML = error.message
+  ul.append(li)
+}
